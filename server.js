9e2ac1be6657a259c3040e9611fae36b4baf5153
Ronier Lima <ronier.lim@gmail.com>
2023-05-13 19:17:48 -0300

    mudando

--- server.js
+++ server.js
@@ -17,3 +17,8 @@ consign()
 app.listen(port, () => {
   console.log(`Servidor online in ${port}`)
 })
+
+app.get("/", (req, res) => {
+  console.log(`teste`)
+  return res.status(200).send({ error: 'Error creating new project.' })
+})
