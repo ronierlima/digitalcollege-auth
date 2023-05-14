d342cc03cca3c5d189b8ffda95d02328e76880e1
Ronier Lima <ronier.lim@gmail.com>
2023-05-13 22:06:52 -0300

    desfazendo a besteira

--- modules/mailer.js
+++ modules/mailer.js
@@ -1,28 +1,9 @@
-const nodemailer = require('nodemailer')
-const { host, port, secure, user } = require('../config/mail.json')
-
+const { MailtrapClient } = require("mailtrap");
 
 const pass = process.env.MAIL_PASS
 
-
-const transporter = nodemailer.createTransport({
-  host,
-  port,
-  secure,
-  auth: {
-    user,
-    pass
-  }
-})
-
-module.exports = transporter
-
-
-const { MailtrapClient } = require("mailtrap");
-
-const TOKEN = "1b7c6bc92014e6b0f8ba450af00314bd";
 const ENDPOINT = "https://send.api.mailtrap.io/";
 
-const client = new MailtrapClient({ endpoint: ENDPOINT, token: TOKEN });
+const client = new MailtrapClient({ endpoint: ENDPOINT, token: pass });
 
 module.exports = client 
