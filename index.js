a02ae0196347fad354bdfbe3d6f8cde2b2566073
Ronier Lima <ronier.lim@gmail.com>
2024-01-17 22:40:48 -0300

    ajusta cors

--- index.js
+++ index.js
@@ -1,63 +1,69 @@
-const express = require('express');
-const bodyParser = require('body-parser')
-const cors = require('cors')
-const path = require('path');
+const express = require("express");
+const bodyParser = require("body-parser");
+const cors = require("cors");
+const path = require("path");
 
-require('dotenv').config()
-require('./config/db')
+require("dotenv").config();
+require("./config/db");
 
 const app = express();
 
-const userController = require('./app/controllers/user');
-const linkController = require('./app/controllers/link');
-const authTokenMiddleware = require('./app/middlewares/authToken');
-const swaggerDocument = require('./config/swagger')
+const userController = require("./app/controllers/user");
+const linkController = require("./app/controllers/link");
+const authTokenMiddleware = require("./app/middlewares/authToken");
+const swaggerDocument = require("./config/swagger");
 
-const port = process.env.PORT || 3000
+const port = process.env.PORT || 3000;
 
-app.use(bodyParser.json())
-app.use(bodyParser.urlencoded({ extended: false }))
-app.use(cors())
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(
+  cors({
+    origin: "*",
+    methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
+    credentials: true,
+    optionsSuccessStatus: 204,
+  })
+);
 
 //ROTAS
-app.use('/swagger-ui', express.static(path.join(__dirname, 'swagger-ui')));
+app.use("/swagger-ui", express.static(path.join(__dirname, "swagger-ui")));
 
-app.get('/swagger.json', (_, res) => {
-
-  res.setHeader('Content-Type', 'application/json');
+app.get("/swagger.json", (_, res) => {
+  res.setHeader("Content-Type", "application/json");
   res.send(swaggerDocument);
 });
 
-app.post('/auth/login', userController.auth)
-app.post('/auth/forgot_password', userController.forgotPassword)
-app.post('/auth/reset_password', userController.resetPassword)
+app.post("/auth/login", userController.auth);
+app.post("/auth/forgot_password", userController.forgotPassword);
+app.post("/auth/reset_password", userController.resetPassword);
 
-app.route('/users')
-  .post(userController.register)
-  .get(userController.listUsers)
+app.route("/users").post(userController.register).get(userController.listUsers);
 
-app.route('/users/:user_id')
+app
+  .route("/users/:user_id")
   .get(userController.getUserById)
   .all(authTokenMiddleware.authenticationJWT)
   .put(userController.updateUserById)
-  .delete(userController.deleteUserById)
+  .delete(userController.deleteUserById);
 
-app.route('/users/:user_id/links')
+app
+  .route("/users/:user_id/links")
   .get(userController.getUserLinks)
   .post(linkController.createLink)
-  .put(linkController.updateLinkById)
+  .put(linkController.updateLinkById);
 
-app.route('/users/:user_id/links/:link_id')
+app
+  .route("/users/:user_id/links/:link_id")
   .get(linkController.getLinkById)
   .all(authTokenMiddleware.authenticationJWT)
   .put(linkController.updateLinkById)
-  .delete(linkController.deleteLinkById)
-
+  .delete(linkController.deleteLinkById);
 
 app.get("/", (_, res) => {
-  return res.status(200).send('Hello World')
-})
+  return res.status(200).send("Hello World");
+});
 
 app.listen(port, () => {
-  console.log(`Servidor online in ${port}`)
-})
+  console.log(`Servidor online in ${port}`);
+});
